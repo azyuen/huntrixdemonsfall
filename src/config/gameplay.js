d42f63bb3206b2a02a408a779241c85a5dd0298b
azyuen <azyuen@gmail.com>
2026-09-11 13:09:45 +1000

    Add centralized gameplay tuning

---
+++ src/config/gameplay.js
@@ -0,0 +1,11 @@
+export const GAMEPLAY = {
+  width: 1280,
+  height: 720,
+  worldWidth: 4200,
+  gravity: 1100,
+  moveSpeed: 300,
+  jumpSpeed: 570,
+  dodgeSpeed: 760,
+  dodgeDuration: 180,
+  dodgeCooldown: 650
+};
