1ef65afa78a3eeb4107f8c1008e245b29247a3b5
azyuen <azyuen@gmail.com>
2026-09-11 15:57:19 +1000

    Register level complete scene

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import { GAMEPLAY } from './config/gameplay.js';
 import BootScene from './scenes/BootScene.js';
 import CharacterSelectScene from './scenes/CharacterSelectScene.js';
 import GameScene from './scenes/GameScene.js';
+import LevelCompleteScene from './scenes/LevelCompleteScene.js';
+import { installRunSummary } from './systems/RunSummaryPatch.js';
+
+installRunSummary(GameScene);
 
 new Phaser.Game({
   type: Phaser.AUTO,
@@ -13,5 +17,5 @@ new Phaser.Game({
   input: { activePointers: 4 },
   physics: { default: 'arcade', arcade: { gravity: { y: GAMEPLAY.gravity }, debug: false } },
   scale: { mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH },
-  scene: [BootScene, CharacterSelectScene, GameScene]
+  scene: [BootScene, CharacterSelectScene, GameScene, LevelCompleteScene]
 });
