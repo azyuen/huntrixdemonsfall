7ec24cac11e004a25fed1db4ee73db7932db5d85
azyuen <azyuen@gmail.com>
2026-09-13 04:34:59 +1000

    Add Rumi idle and run sprite sheet loader

---
+++ src/assets/rumiIdleRunSheet.js
@@ -0,0 +1,5 @@
+import c00 from './rumiSheetChunks/c00.js';
+import c01 from './rumiSheetChunks/c01.js';
+import c02 from './rumiSheetChunks/c02.js';
+
+export const rumiIdleRunSheetDataUrl = `data:image/avif;base64,${c00}${c01}${c02}`;
