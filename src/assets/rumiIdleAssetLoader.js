bde2873acd5572bbf9231aa02617b763227af6da
azyuen <azyuen@gmail.com>
2026-09-12 22:04:04 +1000

    Add production Rumi idle asset loader

---
+++ src/assets/rumiIdleAssetLoader.js
@@ -0,0 +1,20 @@
+import c0 from './rumiIdleChunks/chunk00.js';
+import c1 from './rumiIdleChunks/chunk01.js';
+import c2 from './rumiIdleChunks/chunk02.js';
+import c3 from './rumiIdleChunks/chunk03.js';
+import c4 from './rumiIdleChunks/chunk04.js';
+import c5 from './rumiIdleChunks/chunk05.js';
+import c6 from './rumiIdleChunks/chunk06.js';
+import r0 from './rumiIdleChunks/chunkR00.js';
+import r1 from './rumiIdleChunks/chunkR01.js';
+import r2 from './rumiIdleChunks/chunkR02.js';
+import r3 from './rumiIdleChunks/chunkR03.js';
+import r4 from './rumiIdleChunks/chunkR04.js';
+import r5 from './rumiIdleChunks/chunkR05.js';
+import r6 from './rumiIdleChunks/chunkR06.js';
+
+const encoded=[c0,c1,c2,c3,c4,c5,c6,r0,r1,r2,r3,r4,r5,r6].join('');
+
+export const rumiIdleDataUrl=`data:image/webp;base64,${encoded}`;
+export const RUMI_IDLE_FRAME_WIDTH=216;
+export const RUMI_IDLE_FRAME_HEIGHT=399;
