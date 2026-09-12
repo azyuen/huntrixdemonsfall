1583ae9283d69b0fb4215cf28d3ee4abb3d34192
azyuen <azyuen@gmail.com>
2026-09-12 23:01:43 +1000

    Remove obsolete embedded Rumi idle loader

--- src/assets/finalRumiIdle/index.js
+++
@@ -1,6 +0,0 @@
-import frame0 from './frame0.js';
-import frame1 from './frame1.js';
-import frame2 from './frame2.js';
-import frame3 from './frame3.js';
-
-export const finalRumiIdleFrames=[frame0,frame1,frame2,frame3];
